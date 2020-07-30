bd8718de509ee476979b997d67400d83355e0bdc
Seokjin Yoon <sjy5386@gmail.com>
2020-07-31 01:08:38 +0900

    Add class Server

--- net.js
+++ net.js
@@ -1,51 +1,52 @@
 const tls = require('tls');
 const fs = require('fs');
 
-let server;
-let connections;
-
-const open = (onConnectedCallback, onReceivedCallback) => {
-    connections = [];
-    let i = 0;
-
-    server = tls.createServer({
-        key: fs.readFileSync('privkey.key'),
-        cert: fs.readFileSync('cert.crt')
-    }, socket => {
-        const connection = {
-            id: i++,
-            socket,
-            address: socket.address()
-        };
-        connections.push(connection);
-        console.log(connection.address, `is connected. ${connection.id}`);
-
-        socket.on('data', data => {
-            if (typeof onReceivedCallback == 'function') {
-                onReceivedCallback(connection, data);
-            }
-        }).on('end', () => {
-            console.log(connection.address, `is disconnected. ${connection.id}`);
-            connections.splice(connections.indexOf(connection), 1);
-        }).on('error', err => {
-            console.error(err);
-            connections.splice(connections.indexOf(connection), 1);
-        });
-
-        if (typeof onConnectedCallback == 'function') {
-            onConnectedCallback(connection);
-        }
-    }).on('error', err => {
-        console.error(err);
-    });
+const options = {
+    key: fs.readFileSync('privkey.key'),
+    cert: fs.readFileSync('cert.crt')
 };
 
-const start = port => {
-    server.listen(port, () => {
-        console.log('listen', server.address());
-    });
+const Server = class {
+    constructor(onConnectedCallback, onReceivedCallback) {
+        this.connections = [];
+        let i = 0;
+
+        this.server = tls.createServer(options, socket => {
+            const connection = {
+                id: i++,
+                socket,
+                address: socket.address()
+            };
+            this.connections.push(connection);
+            console.log(connection.address, `is connected. ${connection.id}`);
+
+            socket.on('data', data => {
+                if (typeof onReceivedCallback == 'function') {
+                    onReceivedCallback(connection, data);
+                }
+            }).on('end', () => {
+                console.log(connection.address, `is disconnected. ${connection.id}`);
+                this.connections.splice(this.connections.indexOf(connection), 1);
+            }).on('error', err => {
+                console.error(err);
+                this.connections.splice(this.connections.indexOf(connection), 1);
+            });
+
+            if (typeof onConnectedCallback == 'function') {
+                onConnectedCallback(connection);
+            }
+        }).on('error', err => {
+            console.error(err);
+        });
+    }
+
+    start = port => {
+        this.server.listen(port, () => {
+            console.log('listen', this.server.address());
+        });
+    };
 };
 
 module.exports = {
-    connections, open, start
+    Server
 };
