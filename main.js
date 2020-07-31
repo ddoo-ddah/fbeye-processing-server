fd7df513fdb4ef1711f4405eb101b7b45ab84870
Seokjin Yoon <sjy5386@gmail.com>
2020-07-31 23:30:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+const net = require('./net');
+
 console.log('FBEye Processing Server');
+
+const serverDesktop = new net.Server(null, null);
+const serverMobile = new net.Server(null, null);
+
+serverDesktop.start(10100);
+serverMobile.start(10101);
