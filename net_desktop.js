426c767b874518f319e9ca2d3a8afdac4d1665e2
Seokjin Yoon <sjy5386@gmail.com>
2020-08-01 18:22:50 +0900

    Add settings.json

--- net_desktop.js
+++ net_desktop.js
@@ -1,10 +1,8 @@
 const net = require('./net');
-
-// 포트 번호
-const port = 10100;
+const settings = require('./settings');
 
 const server = new net.Server(connection => {
 }, (connection, data) => {
 });
 
-server.start(port);
+server.start(settings.settings.net.desktop.port);
