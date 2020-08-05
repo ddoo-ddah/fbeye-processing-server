cfbe151db8701996701b99c836ab2436f944f54d
Seokjin Yoon <sjy5386@gmail.com>
2020-08-05 22:35:01 +0900

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,28 +3,30 @@ const settings = require('./settings');
 
 const authCodes = [];
 
-const update = () => {
-    for (let i = 0; i < authCodes.length; i++) {
-        crypto.randomBytes(settings.settings.auth.size, (err, buf) => {
-            if (err) {
-                console.error(err);
-            } else {
-                authCodes[i].authCode = buf;
-            }
+function update() {
+    authCodes.forEach(e => {
+        e.users.forEach(f => {
+            crypto.randomBytes(settings.settings.auth.size, (err, buf) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    f.authCode = buf;
+                }
+            });
         });
-    }
-};
+    });
+}
 
-const verify = (exam, user, authCode) => {
+function verify(exam, user, authCode) {
     return new Promise((resolve, reject) => {
         if (exam && user && authCode && authCodes) {
-            const found = authCodes.find(e => e.authCode == authCode);
-            resolve((found !== undefined) && (found.exam === exam) && (found.user === user));
+            const found = authCodes.find(e => e.exam === exam).users.find(e => e.user === user);
+            resolve((found !== undefined) && (found.authCode === authCodes));
         } else {
             reject(new Error('Failed to verify the auth code.'));
         }
     });
-};
+}
 
 update();
 setInterval(update, settings.settings.auth.interval);
