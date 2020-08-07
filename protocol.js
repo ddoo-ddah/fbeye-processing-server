6723ec4dbdb72c4e32aec4ff011a68ece1e7a9fc
Seokjin Yoon <sjy5386@gmail.com>
2020-08-07 22:12:47 +0900

    Add protocol.js

---
+++ protocol.js
@@ -0,0 +1,14 @@
+const dataTypes = {
+    sin: Buffer.from('SIN'), // Sign IN
+    res: Buffer.from('RES'), // RESponse
+    err: Buffer.from('ERR'), // ERRor
+    inf: Buffer.from('INF'), // INFormation
+    aut: Buffer.from('AUT'), // AUThentication code
+    tes: Buffer.from('TES'), // TESt
+    btn: Buffer.from('BTN'), // BuTtoN coordinate
+    que: Buffer.from('QUE'), // QUEstion
+    ans: Buffer.from('ANS'), // ANSwer
+    key: Buffer.from('KEY'), // KEY
+    eye: Buffer.from('EYE'), // EYE tracking
+    msg: Buffer.from('MSG') // MeSsaGe
+};
