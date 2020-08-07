0ef1b007f2d8f3e3ef16bafc8b2afce7bb9ee4f5
Seokjin Yoon <sjy5386@gmail.com>
2020-08-07 23:11:12 +0900

    Update protocol.js

--- protocol.js
+++ protocol.js
@@ -12,3 +12,21 @@ const dataTypes = {
     eye: Buffer.from('EYE'), // EYE tracking
     msg: Buffer.from('MSG') // MeSsaGe
 };
+
+function toBuffer(obj) {
+    const size = Buffer.allocUnsafe(4);
+    size.writeInt32BE(3 + obj.data.length, 0);
+    return Buffer.concat([size, obj.dataType, obj.data]);
+}
+
+function toObject(buf) {
+    const size = buf.readInt32BE(0);
+    return {
+        dataType: buf.slice(4, 7),
+        data: buf.slice(7, 4 + size)
+    };
+}
+
+module.exports = {
+    dataTypes, toBuffer, toObject
+};
