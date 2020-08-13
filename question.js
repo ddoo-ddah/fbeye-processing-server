c9de839e76c3d54372ffcf5815b0617ffa57a973
Seokjin Yoon <sjy5386@gmail.com>
2020-08-13 18:07:10 +0900

    Update question.js

--- question.js
+++ question.js
@@ -2,58 +2,49 @@ const db = require('./lib/db');
 const crypto = require('./lib/crypto');
 const settings = require('./settings');
 
-const exams = [];
-
-async function loadQuestions(examCode) {
+async function getQuestions(exam) {
     const client = await db.getClient();
     const doc = await client.db().findOne({
-        accessCode: examCode
+        accessCode: exam
     });
+    await client.close();
+    const questions = doc.questions;
     return new Promise((resolve, reject) => {
-        if (doc) {
-            resolve({
-                examCode,
-                questions: doc.questions
-            });
-        } else {
-            reject(new Error('Failed to load questions.'));
-        }
-    });
-}
-
-async function encryptQuestions(exam) {
-    const password = await crypto.randomBytes(settings.settings.crypto.length);
-    const encrypted = await crypto.encrypt(exam.questions, password);
-    return new Promise((resolve, reject) => {
-        if (encrypted) {
-            resolve({
-                examCode: exam.examCode,
-                questions: exam.questions,
-                password
-            });
-        } else {
-            reject(new Error('Failed to encrypt questions.'));
-        }
-    });
-}
-
-async function getQuestions(examCode) {
-    let found = exams.find(e => e.examCode === examCode);
-    if (!found) {
-        const loaded = await loadQuestions(examCode);
-        const encrypted = await encryptQuestions(loaded);
-        exams.push(encrypted);
-        found = encrypted;
-    }
-    return new Promise((resolve, reject) => {
-        if (found) {
-            resolve(found);
+        if (questions) {
+            resolve(questions);
         } else {
             reject(new Error('Failed to get questions.'));
         }
     });
 }
 
+const envelope = new Map();
+
+async function encryptQuestions(questions) {
+    const password = await crypto.randomBytes(settings.settings.crypto.length);
+    const encrypted = await crypto.encrypt(JSON.stringify(questions), password);
+    envelope.set(encrypted, password);
+    return new Promise((resolve, reject) => {
+        if (encrypted) {
+            resolve(encrypted);
+        } else {
+            reject(new Error('Failed to encrypt questions.'));
+        }
+    });
+}
+
+async function getEncryptedQuestions(exam) {
+    const questions = await getQuestions(exam);
+    const encrypted = await encryptQuestions(questions);
+    return new Promise((resolve, reject) => {
+        if (encrypted) {
+            resolve(encrypted);
+        } else {
+            reject(new Error('Failed to get encrypted questions.'));
+        }
+    });
+}
+
 module.exports = {
-    getQuestions
+    envelope, getEncryptedQuestions
 };
