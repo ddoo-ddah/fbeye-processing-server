6c0c48ecc014a253b48fdf3d27abe4740f28a3da
Seokjin Yoon <sjy5386@gmail.com>
2020-08-17 17:55:54 +0900

    Remove question.js

--- exam.js
+++ exam.js
@@ -1,4 +1,6 @@
 const db = require('./lib/db');
+const crypto = require('./lib/crypto');
+const settings = require('./settings');
 
 async function getExamInformation(exam) {
     const client = await db.getClient();
@@ -26,6 +28,62 @@ async function getExamInformation(exam) {
     });
 }
 
+async function getQuestions(exam) {
+    const client = await db.getClient();
+    const doc = await client.db().collection('exams').findOne({
+        accessCode: exam
+    });
+    await client.close();
+    return new Promise((resolve, reject) => {
+        if (doc) {
+            resolve(doc.questions);
+        } else {
+            reject(new Error('Failed to get questions.'));
+        }
+    });
+}
+
+const envelope = new Map();
+
+async function encryptQuestions(questions) {
+    const password = await crypto.randomBytes(settings.settings.crypto.length);
+    const key = await crypto.createKey(password);
+    const encrypted = await crypto.encrypt(JSON.stringify(questions), key, 'utf8');
+    envelope.set(encrypted, key);
+    return new Promise((resolve, reject) => {
+        if (encrypted) {
+            resolve(encrypted);
+        } else {
+            reject(new Error('Failed to encrypt questions.'));
+        }
+    });
+}
+
+async function decryptQuestions(encrypted) {
+    const key = envelope.get(encrypted);
+    const decrypted = await crypto.decrypt(encrypted, key, 'utf8');
+    const questions = JSON.parse(decrypted);
+    return new Promise((resolve, reject) => {
+        if (questions) {
+            resolve(questions);
+        } else {
+            reject(new Error('Failed to decrypt questions.'));
+        }
+    });
+}
+
+async function getEncryptedQuestions(exam) {
+    const questions = await getQuestions(exam);
+    const encrypted = await encryptQuestions(questions);
+    return new Promise((resolve, reject) => {
+        if (encrypted) {
+            resolve(encrypted);
+        } else {
+            reject(new Error('Failed to get encrypted questions.'));
+        }
+    });
+}
+
 module.exports = {
-    getExamInformation
+    getExamInformation, getQuestions, envelope, encryptQuestions, decryptQuestions, getEncryptedQuestions
 };
