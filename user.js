9171822790fe0af2a12f53176956db5a55660ec2
Seokjin Yoon <sjy5386@gmail.com>
2020-08-27 18:31:18 +0900

    Update binary to base64

--- user.js
+++ user.js
@@ -127,7 +127,7 @@ function setMobile(examCode, userCode, connection) {
 
 function updateAuthCode() { // 인증 코드 갱신
     users.forEach(async e => {
-        e.authCode = await crypto.randomBytes(settings.settings.auth.size);
+        e.authCode = (await crypto.randomBytes(settings.settings.auth.size)).toString('base64');
 
         if (e.desktop) { // 갱신된 인증 코드 전송
             e.desktop.write(protocol.toBuffer({
@@ -146,7 +146,7 @@ function verifyAuthCode(examCode, userCode, authCode) {
     return new Promise((resolve, reject) => {
         const found = users.find(e => (e.examCode === examCode) && (e.userCode === userCode));
         if (found && found.authCode) {
-            resolve(authCode.compare(found.authCode) === 0);
+            resolve(authCode === found.authCode);
         } else {
             reject(new Error('Failed to verify the auth code.'));
         }
